fcfa1c7e3583c2aba8cf226954a9de5688ed341c
TONNYSAP <sousaverassap@gmail.com>
2021-10-02 17:35:13 -0300

    Add files via upload

---
+++ darkFlix-myproject/js/play.js
@@ -0,0 +1,12 @@
+
+function assistirFilme() {
+
+    alert("assitir filme");
+
+    window.open('https://www.youtube.com/watch?v=55GxrkdiawM');
+}
+
+function infoFilme() {
+
+    window.open('https://www.adorocinema.com/filmes/filme-44917/criticas/espectadores/');
+}
